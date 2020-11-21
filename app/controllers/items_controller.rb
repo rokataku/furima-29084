4e4e84528a1f27bf391664a848c4210173cf754c
rokataku <t>
2020-11-21 13:51:25 +0900

    新規作成画面の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit()
   end
 
 end
