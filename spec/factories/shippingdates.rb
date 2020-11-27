767372b49c5e4378536410ff288e43f1065bae43
rokataku <t>
2020-11-27 23:25:16 +0900

    rubocupの実行

--- spec/factories/shippingdates.rb
+++ spec/factories/shippingdates.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
   factory :shippingdate do
-    
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.last_name}
-    email                 {Faker::Internet.free_email}
-    password              {"aaa111"}
-    password_confirmation {password}
-    kanjifamilyname       {"渋井"}
-    kanjifirstname        {"拓"}
-    kanafamilyname        {"シブイ"}
-    first_name_kana       {"タク"}
-    birthday              {19880321}
+    nickname              { Faker::Name.last_name }
+    email                 { Faker::Internet.free_email }
+    password              { 'aaa111' }
+    password_confirmation { password }
+    kanjifamilyname       { '渋井' }
+    kanjifirstname        { '拓' }
+    kanafamilyname        { 'シブイ' }
+    first_name_kana       { 'タク' }
+    birthday              { 19_880_321 }
   end
 end
