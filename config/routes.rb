bc7f53612454061c1a04412fb88cb7071f7b340b
rokataku <t>
2020-11-30 23:16:15 +0900

    商品購入画面への変遷

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    resources :consumers
+    resources :consumers, only: [:index, :new, :create]
   end
 end
