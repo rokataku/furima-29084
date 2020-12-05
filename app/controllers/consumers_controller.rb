94d06cb2ba1ebedab4cb6db815565ac27c517119
rokataku <t>
2020-12-05 16:56:37 +0900

    consumerコントローラーの設定

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -1,4 +1,19 @@
 class ConsumersController < ApplicationController
   def index
+    @consumer = Consumer.new
   end
+
+  def new
+  end
+
+  def create
+    @consumer = Consumer.new
+    if @consumer.valid?
+      @consumer.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+  
 end
