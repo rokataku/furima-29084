ced49bf4262f2d4c07a94b27ce8092889779041e
rokataku <t>
2020-12-06 23:30:29 +0900

    Formオブジェクトの設定

---
+++ app/models/consumer_address.rb
@@ -0,0 +1,13 @@
+class ConsumerAddress
+  
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phonenumber, :user_id, :item_id
+  
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank"}
+
+  def save
+    consumer = Consumer.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phonenumber: phonenumber, consumer_id: consumer.id)
+  end
+end
