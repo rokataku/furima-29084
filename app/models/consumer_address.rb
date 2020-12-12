cf95bf8c9e215ce7207257d33e30b6395a29e406
rokataku <t>
2020-12-12 17:11:07 +0900

    テストコードの実装（Formオブジェクト）

--- app/models/consumer_address.rb
+++ app/models/consumer_address.rb
@@ -3,9 +3,21 @@ class ConsumerAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phonenumber, :user_id, :item_id, :token
   
-  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  with_options presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"} do
+    validates :postal_code
+  end
+
+  with_options presence: true, format: {with: /\A\d{11}\z/, message: "is out of setting range"} do
+    validates :phonenumber
+  end
+
+  with_options presence: true do
+    validates :city
+    validates :house_number
+    validates :token
+  end
+
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank"}
-  validates :token, presence: true
 
   def save
     consumer = Consumer.create(user_id: user_id, item_id: item_id)
--- spec/factories/consumer_addresses.rb
+++ spec/factories/consumer_addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :consumer_address do
+    postal_code { '111-1111' }
+    prefecture_id { 1 }
+    city {'豊島区'}
+    house_number { '1-1' }
+    building_name { '目白ハウス' }
+    phonenumber { '12345678910'}
     token {"tok_abcdefghijk00000000000000000"}
   end
 end
