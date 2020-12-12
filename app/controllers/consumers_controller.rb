4162c6fc51950f11e53a21aa76253ad820182a76
rokataku <t>
2020-12-12 13:44:46 +0900

    Payjpの実装

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -1,26 +1,37 @@
 class ConsumersController < ApplicationController
-  def index
-    
-  end
 
-  def new
-    @consumer_address = ConsumerAddress.new
+  def index
+    item = Item.find(params[:item_id])
+    @name = item.name
+    @price = item.price
   end
 
   def create
+    item = Item.find(params[:item_id])
+    @price = item.price
     @consumer_address = ConsumerAddress.new(consumer_params)
-      if @consumer_address.valid?
-        @consumer_address.save
-        redirect_to root_path
-      else
-        render action: :index
-      end
+    if @consumer_address.valid?
+      pay_item
+      @consumer_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
   private
 
   def consumer_params
     params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phonenumber, :item_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_81eb94ce86f8e734eb156c24"
+    Payjp::Charge.create(
+      amount: @price,
+      card: params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/consumer_address.rb
+++ app/models/consumer_address.rb
@@ -1,10 +1,11 @@
 class ConsumerAddress
   
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phonenumber, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phonenumber, :user_id, :item_id, :token
   
   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank"}
+  validates :token, presence: true
 
   def save
     consumer = Consumer.create(user_id: user_id, item_id: item_id)
