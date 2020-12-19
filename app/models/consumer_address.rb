c65ba757afd3e12ff9c9e7625eb787d0ec0ee843
rokataku <t>
2020-12-19 13:57:08 +0900

    バリデーション修正（記述の省略）

--- app/models/consumer_address.rb
+++ app/models/consumer_address.rb
@@ -3,19 +3,10 @@ class ConsumerAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phonenumber, :user_id, :item_id, :token  
 
-  with_options presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"} do
-    validates :postal_code
-  end
-
-  with_options presence: true, numericality: { other_than: 1, message: "can't be blank" } do
-    validates :prefecture_id
-  end
-
-  with_options presence: true, length: { maximum: 11, message: "is out of setting range"} do
-    validates :phonenumber
-  end
-
   with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :phonenumber, length: { maximum: 11, message: "is out of setting range"}
     validates :city
     validates :house_number
     validates :token
